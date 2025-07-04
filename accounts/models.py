60824f3fe279bdd1a8e92fcca288b2bec8c712af
Enock <oswereenock@gmail.com>
2025-07-04 16:24:27 +0300

    custom css

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    profile_image = image = CloudinaryField('image')
+    profile_image = CloudinaryField('image')
 
     is_customer = models.BooleanField(default=True)  # future use: for customer vs admin vs seller etc.
 
